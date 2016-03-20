cac355ed523b81906da2187292f2a0ff20839478
Jeison Triana <jeisontriananr14@hotmail.com>
2016-03-20 13:44:27 -0500

    cambios se agrega formulario de Login registro y vista registro de amor.

---
+++ app/src/main/java/co/edu/udistrital/dulcesparamiamor/LoginActivity.java
@@ -0,0 +1,24 @@
+package co.edu.udistrital.dulcesparamiamor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView lblsingup ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        lblsingup = (TextView)findViewById(R.id.lblsingup);
+
+        lblsingup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
