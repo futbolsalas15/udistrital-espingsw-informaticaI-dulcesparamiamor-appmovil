07868798150fb7e8077ba31bf7d022397a74cf7d
Jeison Triana <jeisontriananr14@hotmail.com>
2016-03-23 15:47:33 -0500

    Se ha modificado el login ahora la aplicación iniciara sesión en el servidor y almacenara los datos del usuario en un sharepreference , al iniciar la aplicación por segunda vez esta no requerira login, tambine se ha modificado el login.

--- app/src/main/java/co/edu/udistrital/dulcesparamiamor/LoginActivity.java
+++ app/src/main/java/co/edu/udistrital/dulcesparamiamor/LoginActivity.java
@@ -3,7 +3,9 @@ package co.edu.udistrital.dulcesparamiamor;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,15 +13,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import Model.UserProfile;
+
 public class LoginActivity extends AppCompatActivity {
     TextView lblsingup ;
     EditText Email,Password;
     Button button;
-
     AlertDialog.Builder builder;
 
+    UserProfile userProfile;
+    SharedPreferences mPrefs ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         lblsingup = (TextView)findViewById(R.id.lblsingup);
@@ -27,6 +34,17 @@ public class LoginActivity extends AppCompatActivity {
         Email = (EditText) findViewById(R.id.txtemail);
         Password = (EditText) findViewById(R.id.txtpassword);
         button = (Button) findViewById(R.id.button);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+       //Linea valida las preferencias si el usuario ya se ha logueado se obtiene el json del usuario y se redirige al Home.
+        Gson gson = new Gson();
+        String json = mPrefs.getString("UserProfile", "");
+        if(!json.equalsIgnoreCase(""))
+        {
+            userProfile = gson.fromJson(json, UserProfile.class);
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        }
+        //Fin Cache del usuario.
 
         lblsingup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,11 +71,11 @@ public class LoginActivity extends AppCompatActivity {
                     AlertDialog alertDialog = builder.create();
                     alertDialog.show();
                 }
-                else
-                {
+                else {
                     BackgroundTask backgroundTask = new BackgroundTask(LoginActivity.this);
-                    backgroundTask.execute("login",Email.getText().toString(),Password.getText().toString());
+                    backgroundTask.execute("login", Email.getText().toString(), Password.getText().toString());
                 }
+
             }
         });
     }
