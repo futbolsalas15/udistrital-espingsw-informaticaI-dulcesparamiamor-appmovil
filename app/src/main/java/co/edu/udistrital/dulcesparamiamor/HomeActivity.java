3f8e67475bd27f9c65c436e9625153f2133e375a
Oscar Bustos <oscaribh2011@gmail.com>
2016-04-17 11:52:04 -0500

    Se modifica el ejemplo anterior con el fin de realizar la funcionalidad, aqui ya se encuentra funcionando, se cre aun boton en el home de la app con el fin de optener el id de registro por parte de GCM, y ya recibi notificaciones. Se procede a integrar esta funcionalidad  en el proceso de registro.

--- app/src/main/java/co/edu/udistrital/dulcesparamiamor/HomeActivity.java
+++ app/src/main/java/co/edu/udistrital/dulcesparamiamor/HomeActivity.java
@@ -4,36 +4,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity {
 
-
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-    private ProgressBar mRegistrationProgressBar;
-    private boolean isReceiverRegistered;
-
+    Button btnRegId;
+    EditText etRegId;
+    GoogleCloudMessaging gcm;
+    String regid;
+    String PROJECT_NUMBER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-      //  mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+        btnRegId = (Button) findViewById(R.id.btnGetRegId);
+        etRegId = (EditText) findViewById(R.id.etRegId);
+
+        btnRegId.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onReceive(Context context, Intent intent) {
-        //        mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-                SharedPreferences sharedPreferences =
-                        PreferenceManager.getDefaultSharedPreferences(context);
-                boolean sentToken = sharedPreferences
-                        .getBoolean("sentTokenToServer", false);
+            public void onClick(View v) {
+                getRegId();
             }
-        };
+        });
     }
 
 
@@ -49,4 +56,33 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(new Intent(HomeActivity.this, CameraActivity.class));
 
     }
+
+    public void getRegId(){
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    PROJECT_NUMBER = getResources().getString(R.string.gcm_SenderId);
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+                    regid = gcm.register(PROJECT_NUMBER);
+                    msg = "Device registered, registration ID=" + regid;
+                    Log.i("GCM", msg);
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                etRegId.setText(msg + "\n");
+            }
+        }.execute(null, null, null);
+    }
+
 }
---
+++ app/src/main/java/co/edu/udistrital/dulcesparamiamor/gcm/GcmMessageHandler.java
@@ -0,0 +1,53 @@
+package co.edu.udistrital.dulcesparamiamor.gcm;
+
+/**
+ * Created by Oscar on 17/04/2016.
+ */
+    import com.google.android.gms.gcm.GoogleCloudMessaging;
+    import android.app.IntentService;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.os.Handler;
+    import android.util.Log;
+    import android.widget.Toast;
+
+public class GcmMessageHandler extends IntentService {
+
+    String mes;
+    private Handler handler;
+    public GcmMessageHandler() {
+        super("GcmMessageHandler");
+    }
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        handler = new Handler();
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        mes = extras.getString("title");
+        showToast();
+        Log.i("GCM", "Received : (" +messageType+")  "+extras.getString("title"));
+
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+
+    }
+
+    public void showToast(){
+        handler.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(),mes , Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
